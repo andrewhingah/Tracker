b127b481a80545fc937e8fdc8af230a7856edaa9
andrewhingah <andrewhinga5@gmail.com>
2018-05-31 16:15:25 +0300

    [chore #2174285] Modify configurations

--- api/instance/config.py
+++ api/instance/config.py
@@ -8,12 +8,22 @@ class DevelopmentConfig(Config):
 	"""configurations for development"""
 	DEBUG=True
 
+class TestingConfig(Config):
+	TESTING = True
+	DEBUG = True
+
 class StagingConfig(Config):
     #Configurations for Staging#
     DEBUG = True
 
+ class ProductionConfig(Config):
+ 	DEBUG = False
+ 	TESTING = False
+
 app_config = {
     'development': DevelopmentConfig,
+    'testing': TestingConfig,
     'staging': StagingConfig,
-    'production': ProductionConfig,
+    'production': ProductionConfig
+   
 }
