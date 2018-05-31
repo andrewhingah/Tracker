1037fa0f67b3090322cd19c30a0bfb94720d6b37
andrewhingah <andrewhinga5@gmail.com>
2018-05-31 16:07:47 +0300

    [chore #2174285] Test cases for creating, editing a request

---
+++ api/tests/test_user_auth.py
@@ -0,0 +1,31 @@
+"""user authentication test cases"""
+
+import json
+import unittest
+from api.app import UserRequest
+
+
+class TestUserAuth(unittest.TestCase):
+    """handles Auth Endpoint test"""
+
+    def setUp(self):
+        UserRequest.testing = True
+        self.app = UserRequest.test_client()
+        self.data = {
+            "username": "Andrew",
+            "password": "password"
+        }
+
+    def test_user_registration(self):
+        """Test for successful user registration"""
+        response = self.app.post('/api/auth/register',
+                                 data=json.dumps(self.data),
+                                 content_type="application/json")
+        result = json.loads(response.data)
+        self.assertEqual(result["status"], "ok")
+        self.assertEqual(result["username"], "Andrew")
+        self.assertEqual(response.status_code, 201)
+
+
+if __name__ == "__main__":
+    unittest.main()
