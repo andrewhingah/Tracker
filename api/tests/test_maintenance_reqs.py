50e0de9b3737cac629154ca98a9833cd00bf6510
andrewhingah <andrewhinga5@gmail.com>
2018-06-03 19:23:25 +0300

    [Chore #2174285] Refactor tests

--- api/tests/test_maintenance_reqs.py
+++ api/tests/test_maintenance_reqs.py
@@ -5,15 +5,26 @@ from model.user_request import user_requests
 
 class UserRequestsTestCase(unittest.TestCase):
 
-    def setUp(self):
+	def setUp(self):
         self.userRequest = {'user1': ['phone1','broken screen']}
         print ('start of test')
 
-    def test_get_all_maintenance_reqs(self):
+    #test a user can get all her/his request
 
-        response = jsonify (user_requests)
-        self.assertEqual(response.status_code, 200)
-        self.assertIn('windows updates and virus protection', str(response.data))
+	def test_get_all_user_reqs(self):
+
+		response = jsonify (user_requests)
+		self.assertEqual(response.status_code, 200)
+		self.assertIn('broken screen', str(response.data))
+
+
+	#test a user can add a request
+
+    def test_add_user_req(self):
+    	response = UserRequestSchema().load(request.get_json())
+    	user_requests.append(response.data)
+    	self.assertEqual(response.status_code, 200)
+    	self.assertIn('windows updates and virus protection', str(response.data))
 
 
 if __name__ == "__main__":
