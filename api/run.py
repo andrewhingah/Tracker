0f5d6adb5e7dbad5429a291ca82ba47a31f2790e
andrewhingah <andrewhinga5@gmail.com>
2018-06-03 19:04:44 +0300

    [Feature #2174285] Run app

--- api/run.py
+++ api/run.py
@@ -1,9 +1,4 @@
-import os
+from cashman.__init__ import app
 
-from app import create_app
-
-config_name = os.getenv('APP_SETTINGS')
-app = create_app(config_name)
-
-if __name__ == '__main__':
-	app.run()
+if __name__ == "__main__":
+    app.run()
