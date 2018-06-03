a45003fa61f468b7eccbee0cf0b075836444e839
andrewhingah <andrewhinga5@gmail.com>
2018-06-03 18:59:29 +0300

    [Feature #2174285] Add API to add a user request

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -1,44 +1,24 @@
-from flask import Flask
-from flask_api import FlaskAPI
+from flask import Flask, jsonify, request
 
-from flask import request, jsonfy, abort
 
-# local import
-from instance.config import app_config
+from model.user_request import user_requests, UserRequest, UserRequestSchema
 
-def create_app(config_name):
-	from api.models import Requests
-    app = FlaskAPI(__name__, instance_relative_config=True)
-    app.config.from_object(app_config[config_name])
-    app.config.from_pyfile('config.py')
+app = Flask(__name__)
+
+
+@app.route('/maintenances')
+def get_maintenances_reqs():
+    schema = UserRequestSchema(many=True)
     
-    return app
+    response = jsonify (user_requests)
+    response.status_code = 200
 
-@app.route('/userRequests/', methods = ['POST', 'GET'])
-def userRequests ():
-	if request.method == 'POST': #POST
-		name = str(request.data.get('name', ''))
-		if name:
-                userRequest = UserRequest(name=name)
-                userRequest.save()
-                response = jsonify({
-                    'name': userRequest[key],
-                    'details': UserRequest['name']
-                })
-                response.status_code = 201
-                return response
-        else:
-            # GET
-            userRequests = models.user_req
+    return response
 
-            for userRequest in userRequests:
-                obj = {
-                    'name': userRequest[key],
-                    'details': userRequest['name']
-                }
-                userRequest.append(obj)
-            response = jsonify(userRequest)
-            response.status_code = 200
-            return response
 
-    return app
+@app.route('/maintenances', methods=['POST'])
+def add_maintenance_req():
+    maintenance = UserRequestSchema().load(request.get_json())
+    
+    user_requests.append(maintenance.data)
+    return "", 204
